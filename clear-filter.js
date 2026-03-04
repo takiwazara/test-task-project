2aa408a6eb9c022931625a56351349c9e724ac1a
Maxim Razdorskiy <darlexxbit@gmail.com>
2026-03-04 17:55:51 +0300

    1

---
+++ clear-filter.js
@@ -0,0 +1,11 @@
+let checkboxList = document.querySelectorAll('.filter__checkbox')
+let clearButton = document.querySelector('.filters__button-clear')
+
+clearButton.addEventListener('click', function () {
+  for (let i = 0; i <
+  checkboxList.length;
+  i++
+) {
+    checkboxList[i].checked = false;
+  }
+})
